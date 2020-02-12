0e79d45ccafcd6a5fe6cd23d325adfec072c1be9
Melle Sieswerda <melle.sieswerda@gmail.com>
2020-02-12 17:49:09 +0100

    Bugfix: previously the docker image 'vantage.chisq' was called instead of 'vtg.chisq'. R packages in r-custom-base docker image now equal to the set needed for vtg.coxph

--- src/R/colnames.R
+++ src/R/colnames.R
@@ -1,6 +1,15 @@
+#' Return column names.
+#'
+#' @param client Instance of \code{vantage.infrastructure::Client}
+#' @param expl_vars \code{Character} vector of variables to include
+#'
+#' @return \code{data.frame} with one row for each site.
+#' @export
+#'
+#' @examples
 colnames <- function(client) {
     writeln("Retrieving column names")
-    image.name <- "harbor.distributedlearning.ai/vantage/vantage.basic:test"
+    image.name <- "harbor.distributedlearning.ai/vantage/vtg.basic:test"
 
     client$set.task.image(
         image.name,
